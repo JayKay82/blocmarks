d45f76e227ce9aa95bf51d67a804604f4b2765ff
Jan Kroezen <jankroezen@me.com>
2015-09-08 19:32:02 +0100

    Fix Mailgun bug

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.smtp_settings = {
-    port:           '587',
-    address:        'smtp.mailgun.com',
+    port:           587,
+    address:        'smtp.mailgun.org',
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
     domain:         'heroku.com',
