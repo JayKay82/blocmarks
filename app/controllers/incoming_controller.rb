71639c6404161ba0169fea474854c893de6cad3c
Jan Kroezen <jankroezen@me.com>
2015-09-11 20:14:53 +0100

    Complete checkpoint 4, Receive Incoming Emails

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,6 +2,7 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
+    head 200
     email = {
       user:  params[:sender],
       topic: params[:subject],
@@ -9,6 +10,5 @@ class IncomingController < ApplicationController
     }
     @bookmark = EmailToBookmark.parse(email)
     @bookmark.save
-    head 200
   end
 end
